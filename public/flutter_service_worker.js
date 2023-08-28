dc632212c711621b038a845e705e255828afe68a
Playcode <playcodestudios@outlook.com>
2023-08-27 20:30:21 -0500

    login refactor

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -38,9 +38,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "55d717edeac91598ee7fdebff6468da7
 "icons/Icon-512.png": "ccefdfa59793c8d5acd8fc01f7ee0085",
 "icons/Icon-maskable-192.png": "49e156b5add01d52c0ed2d01f22d7868",
 "icons/Icon-maskable-512.png": "d29366e82967e23d12953333ceb9ee31",
-"index.html": "442024961a7aa51e2414bb5365cfbd77",
-"/": "442024961a7aa51e2414bb5365cfbd77",
-"main.dart.js": "d597c7020a17794680d6d555fb8a75be",
+"index.html": "d99478097c15806c87fdf60526ed1db8",
+"/": "d99478097c15806c87fdf60526ed1db8",
+"main.dart.js": "c54e2ab7389fdda08102311d81e67d8d",
 "manifest.json": "4c0821aeda911d7e7571708633bc11dc",
 "version.json": "53bf2afc74b55a9a7aad16817be178dc"};
 // The application shell files that are downloaded before a service worker can
