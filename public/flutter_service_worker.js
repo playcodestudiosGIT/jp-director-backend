696201271a415489b35a2ca107daa38294ca2ca6
Playcode <playcodestudios@outlook.com>
2023-09-06 13:54:49 -0500

    fix traduccion front

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "55d717edeac91598ee7fdebff6468da7
 "icons/Icon-maskable-192.png": "f7ce7e1c2c5f9fe4b7f9b50a64dfc806",
 "icons/Icon-maskable-512.png": "f1fae0a640b9569c20395cda34703d92",
 "icons/ic_launcher.png": "0d13fac5f679393411cb2d7215f6e00c",
-"index.html": "6855dd945ac132ed53ccf197fa651c8d",
-"/": "6855dd945ac132ed53ccf197fa651c8d",
-"main.dart.js": "36b268e262f60787ed8c17930dccd3b3",
+"index.html": "93b91394fa6d85f564c9d81306819742",
+"/": "93b91394fa6d85f564c9d81306819742",
+"main.dart.js": "39a3405c524e359db199d09b845ca59d",
 "manifest.json": "5652b3b57228bd4d446cc134bdf55673",
 "version.json": "53bf2afc74b55a9a7aad16817be178dc"};
 // The application shell files that are downloaded before a service worker can
