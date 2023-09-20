51801ae075c72ca784c5298eaae583a34f5b92e6
playcode <playcodestudios@outlook.com>
2023-09-19 19:04:09 -0500

    ultimas correcciones

--- routes/forms.js
+++ routes/forms.js
@@ -27,18 +27,18 @@ router.get('/:id', [
 
 router.post('/', [
     // validarJWT,
-    check('rootform', 'El nombre del formulario es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').not().isEmpty(),
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('phone', 'El telefono es obligatorio').not().isEmpty(),
-    check('business', 'El tipo de business es obligatorio').not().isEmpty(),
-    check('operationyears', 'Los Años de operaciones son obligatorio').not().isEmpty(),
-    check('advertisingbefore', 'Los advertisingbefore son obligatorio').not().isEmpty(),
-    check('facebookurl', 'Los facebookurl son obligatorio').not().isEmpty(),
-    check('instagramurl', 'Los instagramurl son obligatorio').not().isEmpty(),
-    check('advertisinglevel', 'Los advertisinglevel son obligatorio').not().isEmpty(),
-    check('onlineconference', 'Los onlineconference son obligatorio').not().isEmpty(),
-    check('agree', 'Los agree son obligatorio').not().isEmpty(),
+    // check('rootform', 'El nombre del formulario es obligatorio').not().isEmpty(),
+    // check('email', 'El email es obligatorio').not().isEmpty(),
+    // check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    // check('phone', 'El telefono es obligatorio').not().isEmpty(),
+    // check('business', 'El tipo de business es obligatorio').not().isEmpty(),
+    // check('operationyears', 'Los Años de operaciones son obligatorio').not().isEmpty(),
+    // check('advertisingbefore', 'Los advertisingbefore son obligatorio').not().isEmpty(),
+    // check('facebookurl', 'Los facebookurl son obligatorio').not().isEmpty(),
+    // check('instagramurl', 'Los instagramurl son obligatorio').not().isEmpty(),
+    // check('advertisinglevel', 'Los advertisinglevel son obligatorio').not().isEmpty(),
+    // check('onlineconference', 'Los onlineconference son obligatorio').not().isEmpty(),
+    // check('agree', 'Los agree son obligatorio').not().isEmpty(),
     validarCampos
 ], crearFormulario);
 
