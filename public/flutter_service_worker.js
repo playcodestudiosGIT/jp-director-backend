3b73839662013bebb15f26408d396bc4cf344001
playcode <playcodestudios@outlook.com>
2023-10-02 14:52:28 -0500

    add pixel

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -39,8 +39,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "55d717edeac91598ee7fdebff6468da7
 "icons/Icon-maskable-192.png": "f7ce7e1c2c5f9fe4b7f9b50a64dfc806",
 "icons/Icon-maskable-512.png": "f1fae0a640b9569c20395cda34703d92",
 "icons/ic_launcher.png": "0d13fac5f679393411cb2d7215f6e00c",
-"index.html": "ca9f07a69439a4dd932ecc09108bf353",
-"/": "ca9f07a69439a4dd932ecc09108bf353",
+"index.html": "a31be1f2e96c3a5cd1cd77e55b352cf4",
+"/": "a31be1f2e96c3a5cd1cd77e55b352cf4",
 "main.dart.js": "eca403b230856a953ccf20e58e934a87",
 "manifest.json": "5652b3b57228bd4d446cc134bdf55673",
 "version.json": "53bf2afc74b55a9a7aad16817be178dc"};
