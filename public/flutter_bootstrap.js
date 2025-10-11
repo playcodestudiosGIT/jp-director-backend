7ce62fd38c5d697795262433913949fbf5c61f86
playcodestudiosGIT <playcodestudios@gmail.com>
2025-10-11 13:56:12 -0500

    Implementar sistema de logs profesional con Winston y corregir errores de sintaxis

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "952712469"
+    serviceWorkerVersion: "1939831584"
   }
 });
